69186954fcb9ff424b7eda6d96402f428ad6f554
Guerchôm Doré <d.homger@gmail.com>
2018-08-22 12:32:16 +0200

    Somme files for project, gd_Signature.js is now usefull

--- gd_Signature.js
+++ gd_Signature.js
@@ -1,101 +1,115 @@
+'use strict';
+class gd_Point{
+    constructor(x = 0, y = 0){
+        this.x = x;
+        this.y = y;
+    }
+
+}
 
 class gd_Signature{
-    constructor(container, width = "300px", heigth = "200px"){
-        this.signatureCanvas = document.createElement("canvas");
-        this.width = width;
-        this.heigth = heigth;
+    constructor(container){
         this.container = container;
-        this.container.appendChild(this.signatureCanvas);
+        this.memberSetup();
+        
+    }
+
+    memberSetup(){
+        let cach = ["relative", "absolute", "fixed"];
+        let set = true;
+        for(let i in cach){
+            if($(this.container).css("position") == cach[i] ){
+                set =false;
+                break;
+            }
+        }
+        if(set){
+            $(this.container).css("position","relative");
+        }
+
+
+        this.mousedown = false;
+        this.previousPos = new gd_Point(0,0);
+        this.currentPos = new gd_Point(0,0);
+
+
+        this.canvas = document.createElement("canvas");
+        $(this.container).append(this.canvas);
+        this.context = this.canvas.getContext("2d");
+
+        this.setCanvasDimension();
+
+        this.mousedown_function = this.mousedown_function.bind(this);
+        this.mousemove_function = this.mousemove_function.bind(this);
+        this.mouseup_function = this.mouseup_function.bind(this);
+        this.mouseleave_function = this.mouseleave_function.bind(this);
+        
+        $(this.canvas).mousedown(this.mousedown_function);
+        $(this.canvas).mousemove(this.mousemove_function);
+        $(this.canvas).mouseup(this.mouseup_function);
+        $(this.canvas).mouseleave(this.mouseleave_function);
+
+        this.setDrawStyle();
+        this.context.fillStyle = "#FFFFFF";
+        this.clear();
+    }
+    
+    setCanvasDimension(){
+        this.canvasWidth = $(this.container).width();
+        this.canvasHeight = $(this.container).height();
+        $(this.canvas).attr({
+            "width": this.canvasWidth,
+            "height": this.canvasHeight,
+        });
+        this.canvasOffset = $(this.canvas).offset();
+    }
+
+    clear(){
+        this.context.fillRect(0,0, this.canvasWidth, this.canvasHeight);
+    }
+
+    mousedown_function(event){
+        console.log("DOWN");
+        this.mousedown = true;
+        this.currentPos.x = event.pageX - this.canvasOffset.left;
+        this.currentPos.y = event.pageY - this.canvasOffset.top;
+    }
+    
+    mousemove_function(event){
+        console.log("MOVE");
+        if(this.mousedown == true){
+            console.log("MOVE T");
+            this.previousPos.x = this.currentPos.x;
+            this.previousPos.y = this.currentPos.y;
+
+            this.currentPos.x = event.pageX - this.canvasOffset.left;
+            this.currentPos.y = event.pageY - this.canvasOffset.top;
+
+            this.draw();
+            console.log("x: " + this.currentPos.x + "    y: " + this.currentPos.y);
+        }
+    }
+
+    mouseup_function(){
+        this.mousedown = false;
+        console.log("UP");
+    }
+
+    mouseleave_function(){
+        console.log("LEAVE");
+        this.mousedown = false;
     }
 
     draw(){
-        this.context.moveTo(this.from.x, this.from.y);
-        this.context.lineTo(this.to.x, this.to.y);
+        this.context.beginPath();
+        this.context.moveTo(this.previousPos.x, this.previousPos.y);
+        this.context.lineTo(this.currentPos.x, this.currentPos.y);
         this.context.stroke();
-        console.log("draw: x: " + this.to.x + "  y: "+ this.to.y);
     }
+    setDrawStyle(){
 
-    signatureCanvasSetUp(){
-    
-        this.signatureCanvas.style.width = this.width;
-        this.signatureCanvas.style.heigth = this.heigth;
-        this.context = this.signatureCanvas.getContext("2d");
-    
-        this.context.strokeStyle = "black";
-        this.context.lineWidth = "5px";
-        this.context.lineJoin = "round";
         this.context.lineCap = "round";
-    
-        this.canvasOffset.left = $(signatureCanvas).offset().left;
-        this.canvasOffset.top = $(signatureCanvas).offset().top;
-        
-        $(signatureCanvas).mousedown(function(event){
-            mousedown = true;
-            mouseCurrentPosition.x = event.pageX - canvasOffset.left;
-            mouseCurrentPosition.y = event.pageY - canvasOffset.top;
-    
-        });
-        $(signatureCanvas).mouseup(function(){
-            mousedown = false;
-        });
-        $(signatureCanvas).mouseleave(function(){
-            mousedown = false;
-        });
-        $(signatureCanvas).mousemove(function(event){
-            if(mousedown){
-                mousePreviousPosition.x = mouseCurrentPosition.x;
-                mousePreviousPosition.y = mouseCurrentPosition.y;
-                mouseCurrentPosition.x = event.pageX - canvasOffset.left;
-                mouseCurrentPosition.y = event.pageY - canvasOffset.top;
-                draw(context, mousePreviousPosition, mouseCurrentPosition);
-            }
-        });
+        this.context.lineJoin = "round";
+        this.context.lineWidth = "2px";
     }
-
-
-}
-
-
-signatureCanvasSetUp() {
-    
-    $(signatureCanvas).attr("width", $(signatureCanvas).parent().attr("width") + "px");
-    $(signatureCanvas).attr("height", $(signatureCanvas).parent().attr("height") + "px");
-    let context = signatureCanvas.getContext("2d");
-
-    context.strokeStyle = "black";
-    context.lineWidth = "5px";
-    context.lineJoin = "round";
-    context.lineCap = "round";
-
-    canvasOffset.left = $(signatureCanvas).offset().left;
-    canvasOffset.top = $(signatureCanvas).offset().top;
-    
-    $(signatureCanvas).mousedown(function(event){
-        mousedown = true;
-        mouseCurrentPosition.x = event.pageX - canvasOffset.left;
-        mouseCurrentPosition.y = event.pageY - canvasOffset.top;
-
-    });
-    $(signatureCanvas).mouseup(function(){
-        mousedown = false;
-    });
-    $(signatureCanvas).mouseleave(function(){
-        mousedown = false;
-    });
-    $(signatureCanvas).mousemove(function(event){
-        if(mousedown){
-            mousePreviousPosition.x = mouseCurrentPosition.x;
-            mousePreviousPosition.y = mouseCurrentPosition.y;
-            mouseCurrentPosition.x = event.pageX - canvasOffset.left;
-            mouseCurrentPosition.y = event.pageY - canvasOffset.top;
-            draw(context, mousePreviousPosition, mouseCurrentPosition);
-        }
-    });
-}
-
-draw(context, from, to){
-    context.moveTo(from.x, from.y);
-    context.lineTo(to.x, to.y);
-    context.stroke();
-    console.log("draw: x: " + to.x + "  y: "+ to.y);
 }
